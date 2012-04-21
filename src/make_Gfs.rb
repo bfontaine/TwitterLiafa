950d65edb5a62b523eafe3e7d5528a91d0c3e050
Baptiste Fontaine <batifon@yahoo.fr>
2012-04-21 23:27:37 +0200

    make_Gfs implemented

--- src/make_Gfs.rb
+++ src/make_Gfs.rb
@@ -1,5 +1,7 @@
 #! /usr/bin/ruby1.9.1
+# -*- coding: utf-8 -*-
 
+require '../../../Programmation/Github/Graph.rb/gdf'
 require 'json'
 
 # -- configuration
@@ -8,6 +10,10 @@ Friends_ids_dir = '../json-utils/friends_ids'
 Output_dir = '../graphs/Gf'
 # -- /configuration
 
+names_to_ids = JSON.parse(File.read('../json-utils/cache_ids.json'))
+ids_to_names = {}
+
+names_to_ids.each_pair { |n,i| ids_to_names[i] = n }
 
 if !Dir.exists?(Output_dir)
     Dir.mkdir Output_dir
@@ -26,3 +32,42 @@ def get_friends_ids(name)
         return []
     end
 end
+
+users = JSON.parse(File.read('../json-utils/all_usernames_uniq.json'))
+
+users_friends = {}
+
+puts 'Getting users friends names…'
+users.each { |u|
+    ids = get_friends_ids(u)
+    li = []
+    ids.each{ |i|
+        n = ids_to_names[i];
+        li << n if (!n.nil?)
+    }
+    users_friends[u] = li
+}
+
+puts 'Making Gfs…'
+('001'..'124').each { |n|
+
+    g = GDF::load("../graphs/#{n}_previous.gdf")
+    # delete edges, keep nodes
+    g.edges = GDF::Graph::EdgeArray.new
+    g.edges.set_default({'directed' => true}) #FIXME not used
+
+    g_users = g.nodes.map {|n| n['name']}
+
+    g_users.each { |u|
+
+        friends = users_friends[u]
+    
+        friends.each { |f|
+            if (g_users.include?(f))
+                g.edges.push({'node1'=>u, 'node2'=>f})
+            end
+        }
+    }
+
+    g.write("#{Output_dir}/#{n}_gf.gdf")
+}
